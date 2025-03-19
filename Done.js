8a70260109739344ccfffc88e90ecc7719d71ee2
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-03-19 14:55:35 +0000

    recoding it

--- Done.js
+++ Done.js
@@ -4,7 +4,7 @@
  */
 
 /* Global Variables */
-var trainer = false 
+var trainer = false
 var count = 0
 /** 
  * main  
@@ -13,12 +13,12 @@ var count = 0
  * @return none
  */
 /* Main */
-function main() {
-    let again = true 
-    trainer = confirm ("trainer mode?")
-    playNim
-    again = confirm ("do you want to play again?")
-if (again == true) { main()}
+function main(){
+    let again = false
+    trainer = confirm ("Trainer mode?") 
+    playNim()
+    again = confirm ("Would you like to play again?")
+if (again == true) main()
 else alert ("thanks for playing")
 }
 
@@ -28,16 +28,15 @@ else alert ("thanks for playing")
  * @param none 
  * @return none
  */
+function playNim(){
 count = 0
 while (count < 21) {
     userTurn()
-    if (count > 20)
-    alert ("you lose")
-    
+    if (count > 20) {alert ("you lose")}
     else {
     cpuTurn()
-    if (count > 20)
-    alert ("you win")
+    if (count > 20) {alert ("you win")} 
+        }   
     }
 }
 
@@ -47,11 +46,29 @@ while (count < 21) {
  * @param none 
  * @return none
  */
+function userTurn(){
+let turn = parseInt ( prompt ("enter a 1, 2 or 3" ) )
 
+if (turn < 1 || turn > 3) {
+    alert ("your input is invalid")
+    userTurn()
+}
+else count = count + turn 
+
+}
 /** 
  * cpuTurn 
  * Generate computer's turn without losing on purpose.  Different turns if trainer or simple.  
  * @param none 
  * @return none
  */
-
+function cpuTurn(){
+turn = 0 
+if (count == 17) turn = 3 
+else if (count == 18) turn = 2
+else if (count > 18) turn = 1
+else if (trainer == true) turn = 4 - count % 4
+else turn = Math.floor(Math.random()*3+1)
+count += turn 
+alert ("I counted " + turn + " the count is now " + count)
+}
