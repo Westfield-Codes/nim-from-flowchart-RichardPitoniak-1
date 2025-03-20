fd9e908ef3b0cf1d52f4b1d09f55ab3d7ebb4a1e
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-03-20 14:44:22 +0000

    turn order choice done

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 /* Global Variables */
 var trainer = false 
 var count = 0
+userFirst = false 
 /** 
  * main  
  * Handles new Nim games with gametype choice simple or trainer and a play again option. 
@@ -16,7 +17,7 @@ var count = 0
 function main() {
     let again = true 
     trainer = confirm ("trainer mode?")
-    playNim
+    playNim()
     again = confirm ("do you want to play again?")
 if (again == true) { main()}
 else alert ("thanks for playing")
@@ -28,18 +29,35 @@ else alert ("thanks for playing")
  * @param none 
  * @return none
  */
+function playNim() {
 count = 0
+userFirst = confirm ("would you like to go first?")
+if (userFirst == true) {
 while (count < 21) {
     userTurn()
     if (count > 20)
     alert ("you lose")
-    
     else {
     cpuTurn()
     if (count > 20)
     alert ("you win")
     }
 }
+}
+else {
+    while (count < 21) {
+        cpuTurn()
+        if (count > 20)
+        alert ("you win")
+        else {
+        userTurn()
+        if (count > 20)
+        alert ("you lose")
+        }
+}
+}
+}
+
 
 /** 
  * userTurn  
@@ -47,11 +65,37 @@ while (count < 21) {
  * @param none 
  * @return none
  */
-
+function userTurn(){
+let turn = parseInt ( prompt("enter a 1 a 2 or a 3"))
+if (turn < 1 || turn > 3) {
+    alert ("invalid input")
+    userTurn()
+}
+else count = count + turn
+}
 /** 
  * cpuTurn 
  * Generate computer's turn without losing on purpose.  Different turns if trainer or simple.  
  * @param none 
  * @return none
  */
-
+function cpuTurn() {
+    if (userFirst == true){
+    if (count == 17 ) turn = 3
+    else if (count == 18 ) turn = 2
+    else if (count > 18 ) turn = 1
+    else if (trainer == true ) turn = 4 - count % 4
+    else turn = Math.floor(Math.random()*3+1)
+    count += turn 
+    alert ("I counted " + turn + " count is now " + count)
+    }
+    else {
+    if (count == 17 ) turn = 3
+    else if (count == 18 ) turn = 2
+    else if (count > 18 ) turn = 1
+    else if (trainer == true ) turn = Math.floor(Math.random()*3+1)
+    else turn = Math.floor(Math.random()*3+1)
+    count += turn 
+    alert ("I counted " + turn + " count is now " + count)
+    }
+}
